c7d5af9bd8c3fb3f50e90c6eac304283ee078679
Guganesh <guganesh99@gmail.com>
2021-10-15 21:36:09 +0800

    Created new component AlzheimersGame

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="IS216 Project" msg1="WAD 2 Project"/> 
+  <HelloWorld msg="IS216 Project" msg1="WAD 2 Project"/>
+  <AlzheimersGame/>
   <!-- component called like HTML tag, can use variables as placeholders-->
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import AlzheimersGame from './components/alzh_game.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    AlzheimersGame
   }
 }
 </script>
---
+++ src/components/alzh_game.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>I am alzh game</div>
+</template>
+
+<script>
+export default {
+  name: 'AlzheimersGame',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
