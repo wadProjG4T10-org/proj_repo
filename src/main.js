94a43240d4c54dd1046d947c2bc5a77c4698c53c
Guganesh <64139456+guga-nesh@users.noreply.github.com>
2021-10-17 20:39:06 +0800

    Added in FallGame and implemented Phaser into views (#3)

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import { createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { defineCustomElements as defineIonPhaser } from '@ion-phaser/core/loader'
 
+defineIonPhaser(window);
 
-
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.config.compilerOptions.isCustomElement = tag => tag.startsWith('ion-') 
+app.use(router)
+app.mount('#app')
