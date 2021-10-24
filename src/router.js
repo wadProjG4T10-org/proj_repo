ec0818ec98069696c016cd8cf96841d802aef37a
Hazyra <hazyrams.2020@scis.smu.edu.sg>
2021-10-24 15:14:47 +0800

    added snake game route and tidied up indents

--- src/router.js
+++ src/router.js
@@ -3,12 +3,15 @@ import Home from './views/Home.vue'
 import AlzheimersGame from './views/AlzhGame.vue'
 import Games from './views/Games.vue'
 import FallGame from './views/FallGame.vue'
+import SnakeGame from './views/snakeGame.vue'
+
 
 const routes = [
     { path: '/', component: Home, name: "Home" },
     { path: '/games', component: Games, name: "Games" },
     { path: '/games/alzh', component: AlzheimersGame, name: "Alzheimer's Game" },
     { path: '/games/fall', component: FallGame, name: "Fall Game" },
+    { path: '/games/diabetes', component: SnakeGame, name: "Snake Game" },
 ]
 
 export default createRouter({
