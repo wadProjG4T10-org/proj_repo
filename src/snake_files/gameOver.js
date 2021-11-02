50a23af215334d07c45f5d47ed09169bb0b02fd5
Hazyra <hazyrams.2020@scis.smu.edu.sg>
2021-11-02 21:37:04 +0800

    snake game edit

--- src/snake_files/gameOver.js
+++ src/snake_files/gameOver.js
@@ -1,27 +1,51 @@
 import Phaser from "phaser";
 
+// List of sentences to put into an array
+var diabetes_facts = ['Type 1 Diabetes is when your body stops making insulin, the key to let the blood sugar into your body cells for energy. It can be found in children, teens and young adults.',
+                      'There is no prevention for Type 1 diabetes.',
+                      'Type 2 diabetes is developed over many years, can also be found in children, teens and young adults.',
+                      'Type 2 diabetes prevention: Healthy Lifestyle changes, losing weight healthily, eating healthy food, be active!',
+                      'The risk of getting type 2 diabetes increases as you get older.',
+                      'Did you know that there are 4 types of diabetes? Type 1, Type 2, Gestational and Prediabetes.',
+                      'Gestational diabetes develops in pregnant women who have never had diabetes.',
+                      'Did you know that a baby could have higher risk for health problems if the mommy have gestational diabetes?',
+                      'Prediabetes can occur anytime, and their blood sugar levels are higher than normal!',
+                      'Prediabetes raises your risk of getting type 2 diabetes, heart disease and stroke.',
+                      'Diabetes is caused by high blood sugar levels. Eat your snacks moderately as a diet in high level of sugar and fat content can lead to obesity and increase your risk of getting one!',
+                      'Did you know, once you get diabetes, you cannot be cured? So please be healthy and consume your sugars and fat moderately!',
+                      'Did you know, if you do not take care of your health when you are a diabetic (person with diabetes) there is a chance of getting your joints amputated (chopped off)? So, take care of your health!',
+                      'Did you know that diabetics (person with diabetes) cannot eat too many carbohydrates like white rice? You would need to talk to your doctor about your diet. So if you like your white rice a lot, take care of your health!',
+                      'Diabetes can cause so many issues: Heart disease, Stroke, Kidney Disease / Kidney Failure, Nerve damage, eye disease, digestive problems',
+                      'Diabetes can also be caused by genetics, but there is no exact cause, no cure or any known prevention.',
+                      'Remember! Eating well, staying active is the best prevention to diseases.'
+                    ];
+
+var random_facts = Math.floor((Math.random() * diabetes_facts.length));
+
+
 export default class gameOver extends Phaser.Scene {
-    constructor() {
-        super('gameOver')
-    }
+
     preload() {
-        this.load.image('gameover', './assets/gameover.png');
+
     }
 
     create() {
-        this.add.button(0,0,'gameover',this.scene,this);
+        // this.add.button(0,0,'gameover',this.scene,this);
 
-        this.add.text(300,250,"LAST SCORE",{font:"bolder 24px san-serif", fill: "#ffb00", align:"center"});
-        this.add.text(300,250, score.toString(),{font:"bolder 26px san-serif", fill: "#fff", align:"center"});
+        // this.add.text(300,250,"LAST SCORE",{font:"bolder 24px san-serif", fill: "#ffb00", align:"center"});
+        // this.add.text(300,250, score.toString(),{font:"bolder 26px san-serif", fill: "#fff", align:"center"});
 
         // GAME OVER (CAN'T FIX): still cannot have the overlay)
         // this.gameOverText = this.add.text(275, 250, `Game Over`, { fontSize: '32px', fill: '#000' }).setOrigin(0.5);
         // this.gameOverText.visible = false;
+        this.add.text(235, 300, `${diabetes_facts[random_facts]}`, {
+            fontSize: '32px',
+            fontFamily: "../fonts/PressStart2P-Regular.ttf" 
+        })
+        .setOrigin(0.0,0.0)
+
 
     }
 
-    startGame() {
-        this.scene.start('snakeGame')
-    }
 
 }
--- src/snake_files/snakeGame.js
+++ src/snake_files/snakeGame.js
@@ -14,25 +14,25 @@ var DOWN = 1;
 var LEFT = 2;
 var RIGHT = 3;
 
-// List of sentences to put into an array
-var diabetes_facts = ['Type 1 Diabetes is when your body stops making insulin, the key to let the blood sugar into your body cells for energy. It can be found in children, teens and young adults.',
-                      'There is no prevention for Type 1 diabetes.',
-                      'Type 2 diabetes is developed over many years, can also be found in children, teens and young adults.',
-                      'Type 2 diabetes prevention: Healthy Lifestyle changes, losing weight healthily, eating healthy food, be active!',
-                      'The risk of getting type 2 diabetes increases as you get older.',
-                      'Did you know that there are 4 types of diabetes? Type 1, Type 2, Gestational and Prediabetes.',
-                      'Gestational diabetes develops in pregnant women who have never had diabetes.',
-                      'Did you know that a baby could have higher risk for health problems if the mommy have gestational diabetes?',
-                      'Prediabetes can occur anytime, and their blood sugar levels are higher than normal!',
-                      'Prediabetes raises your risk of getting type 2 diabetes, heart disease and stroke.',
-                      'Diabetes is caused by high blood sugar levels. Eat your snacks moderately as a diet in high level of sugar and fat content can lead to obesity and increase your risk of getting one!',
-                      'Did you know, once you get diabetes, you cannot be cured? So please be healthy and consume your sugars and fat moderately!',
-                      'Did you know, if you do not take care of your health when you are a diabetic (person with diabetes) there is a chance of getting your joints amputated (chopped off)? So, take care of your health!',
-                      'Did you know that diabetics (person with diabetes) cannot eat too many carbohydrates like white rice? You would need to talk to your doctor about your diet. So if you like your white rice a lot, take care of your health!',
-                      'Diabetes can cause so many issues: Heart disease, Stroke, Kidney Disease / Kidney Failure, Nerve damage, eye disease, digestive problems',
-                      'Diabetes can also be caused by genetics, but there is no exact cause, no cure or any known prevention.',
-                      'Remember! Eating well, staying active is the best prevention to diseases.'
-                    ];
+// // List of sentences to put into an array
+// var diabetes_facts = ['Type 1 Diabetes is when your body stops making insulin, the key to let the blood sugar into your body cells for energy. It can be found in children, teens and young adults.',
+//                       'There is no prevention for Type 1 diabetes.',
+//                       'Type 2 diabetes is developed over many years, can also be found in children, teens and young adults.',
+//                       'Type 2 diabetes prevention: Healthy Lifestyle changes, losing weight healthily, eating healthy food, be active!',
+//                       'The risk of getting type 2 diabetes increases as you get older.',
+//                       'Did you know that there are 4 types of diabetes? Type 1, Type 2, Gestational and Prediabetes.',
+//                       'Gestational diabetes develops in pregnant women who have never had diabetes.',
+//                       'Did you know that a baby could have higher risk for health problems if the mommy have gestational diabetes?',
+//                       'Prediabetes can occur anytime, and their blood sugar levels are higher than normal!',
+//                       'Prediabetes raises your risk of getting type 2 diabetes, heart disease and stroke.',
+//                       'Diabetes is caused by high blood sugar levels. Eat your snacks moderately as a diet in high level of sugar and fat content can lead to obesity and increase your risk of getting one!',
+//                       'Did you know, once you get diabetes, you cannot be cured? So please be healthy and consume your sugars and fat moderately!',
+//                       'Did you know, if you do not take care of your health when you are a diabetic (person with diabetes) there is a chance of getting your joints amputated (chopped off)? So, take care of your health!',
+//                       'Did you know that diabetics (person with diabetes) cannot eat too many carbohydrates like white rice? You would need to talk to your doctor about your diet. So if you like your white rice a lot, take care of your health!',
+//                       'Diabetes can cause so many issues: Heart disease, Stroke, Kidney Disease / Kidney Failure, Nerve damage, eye disease, digestive problems',
+//                       'Diabetes can also be caused by genetics, but there is no exact cause, no cure or any known prevention.',
+//                       'Remember! Eating well, staying active is the best prevention to diseases.'
+//                     ];
 
 export default class snakeGame extends Phaser.Scene {
 
@@ -179,16 +179,15 @@ export default class snakeGame extends Phaser.Scene {
 
                         if (hitBody)
                         {
-                            // FACTS (change it to make it end Screen)
-                            var random_facts = Math.floor((Math.random() * diabetes_facts.length));
-                            alert (diabetes_facts[random_facts]);
+                            // FACTS (change it to make it end Screen) Try not to use this here, maybe after showcasing the facts then reload.
+                            // var random_facts = Math.floor((Math.random() * diabetes_facts.length));
+                            // alert (diabetes_facts[random_facts]);
                             
-                            // Try not to use this here, maybe after showcasing the facts then reload.
-                            location.reload(); 
+                            // location.reload(); 
                             
                             this.alive = false;
 
-                            this.input.on('pointerdown', () => this.scene.start('gameOver'));
+                            // this.input.on('pointerdown', () => this.scene.start('gameOver'));
 
                             return false;
                         }
