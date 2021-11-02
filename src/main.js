8b4b63aa05542ae413da8357492226d5d4682497
avigale <80951408+avigale@users.noreply.github.com>
2021-11-02 19:27:32 +0800

    map

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import router from './router'
 import { defineCustomElements as defineIonPhaser } from '@ion-phaser/core/loader'
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 defineIonPhaser(window);
 
+
 const app = createApp(App)
-app.config.compilerOptions.isCustomElement = tag => tag.startsWith('ion-') 
+app.config.compilerOptions.isCustomElement = tag => tag.startsWith('ion-')
 app.use(router)
 app.mount('#app')
--- src/router.js
+++ src/router.js
@@ -3,12 +3,16 @@ import Home from './views/Home.vue'
 import AlzheimersGame from './views/AlzhGame.vue'
 import Games from './views/Games.vue'
 import FallGame from './views/FallGame.vue'
+import Locator from './views/Locator.vue'
+
 
 const routes = [
     { path: '/', component: Home, name: "Home" },
     { path: '/games', component: Games, name: "Games" },
     { path: '/games/alzh', component: AlzheimersGame, name: "Alzheimer's Game" },
     { path: '/games/fall', component: FallGame, name: "Fall Game" },
+    { path: '/map', component: Locator, name: "Locator"},
+
 ]
 
 export default createRouter({
