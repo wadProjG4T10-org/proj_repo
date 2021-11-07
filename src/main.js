39963dd154ac243233957c8423902b805730d9b8
Guganesh <guganesh99@gmail.com>
2021-11-07 16:58:54 +0800

    fixed bootstrap

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import { defineCustomElements as defineIonPhaser } from '@ion-phaser/core/loader'
 
-// import 'bootstrap'
-// import 'bootstrap/dist/css/bootstrap.min.css'
+
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 defineIonPhaser(window);
 
@@ -12,4 +14,5 @@ defineIonPhaser(window);
 const app = createApp(App)
 app.config.compilerOptions.isCustomElement = tag => tag.startsWith('ion-')
 app.use(router)
+app.use(BootstrapVue3)
 app.mount('#app')
