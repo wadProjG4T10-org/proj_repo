c616038fdc10a9db8d1accb792a37f38da700dc9
Hazyra <hazyrams.2020@scis.smu.edu.sg>
2021-11-08 14:54:58 +0800

    added a simple design for the games menu

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,14 +1,23 @@
 <template>
-    <div>Game Menu</div>
-    <section class="cards-container">
-      <GameCard gameName="Alzheimer's Game" 
-        gameDescription="This is a description about Alzheimer's Game which is very very very long."
-        route="/games/alzh"  />
+    <div class="container" style="margin-top: 20vh;">
+      
+      <b-row class="mt-5" style="max-width: 20rem; margin: auto;">
+        <div style="font-family: myFirstFont;" >Game Menu</div>
+      </b-row>
 
-      <GameCard gameName="Snake Game" 
-        gameDescription="Have fun with our Retro Snake Game and learn more about Diabetes through your journey!"
-        route="/games/diabetes"  />
-    </section>
+      <b-row class="my-5" style="max-width: 20rem; margin: auto;">
+        <GameCard gameName="Alzheimer's Game" 
+          gameDescription="This is a description about Alzheimer's Game which is very very very long."
+          route="/games/alzh"  />
+      </b-row>    
+
+      <b-row class="my-5" style="max-width: 20rem; margin: auto;">
+        <GameCard gameName="Snake Game" 
+          gameDescription="Have fun with our Retro Snake Game and learn more about Diabetes through your journey!"
+          route="/games/diabetes"  />
+      </b-row>
+        
+    </div>
 </template>
 
 <script>
@@ -25,10 +34,8 @@ export default {
 </script>
 
 <style>
-.cards-container {
-  margin-top: 4rem;
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  row-gap: 1rem;
-}
+  @font-face {
+    font-family: myFirstFont;
+    src: url('../fonts/PressStart2P-Regular.ttf');
+  }
 </style>
