78cb36c84953b9c72a961ac2aa90c1ba755ff544
Hazyra <91420160+Hazyra@users.noreply.github.com>
2021-11-08 10:06:57 +0800

    Snake screen (#20)
    * fixed score for snakeGame

--- src/snake_files/gameOver.js
+++ src/snake_files/gameOver.js
@@ -1,27 +1,79 @@
 import Phaser from "phaser";
 
 export default class gameOver extends Phaser.Scene {
+
     constructor() {
         super('gameOver')
     }
+
     preload() {
-        this.load.image('gameover', './assets/gameover.png');
+
     }
 
     create() {
-        this.add.button(0,0,'gameover',this.scene,this);
 
-        this.add.text(300,250,"LAST SCORE",{font:"bolder 24px san-serif", fill: "#ffb00", align:"center"});
-        this.add.text(300,250, score.toString(),{font:"bolder 26px san-serif", fill: "#fff", align:"center"});
+        var diabetes_facts = ['Type 1 Diabetes is when your body stops making insulin, the key to let the blood sugar into your body cells for energy. It can be found in children, teens and young adults.',
+                      'There is no prevention for Type 1 diabetes.',
+                      'Type 2 diabetes is developed over many years, can also be found in children, teens and young adults.',
+                      'Type 2 diabetes prevention: Healthy Lifestyle changes, losing weight healthily, eating healthy food, be active!',
+                      'The risk of getting type 2 diabetes increases as you get older.',
+                      'Did you know that there are 4 types of diabetes? Type 1, Type 2, Gestational and Prediabetes.',
+                      'Gestational diabetes develops in pregnant women who have never had diabetes.',
+                      'Did you know that a baby could have higher risk for health problems if the mommy have gestational diabetes?',
+                      'Prediabetes can occur anytime, and their blood sugar levels are higher than normal!',
+                      'Prediabetes raises your risk of getting type 2 diabetes, heart disease and stroke.',
+                      'Diabetes is caused by high blood sugar levels. Eat your snacks moderately as a diet in high level of sugar and fat content can lead to obesity and increase your risk of getting one!',
+                      'Did you know, once you get diabetes, you cannot be cured? So please be healthy and consume your sugars and fat moderately!',
+                      'Did you know, if you do not take care of your health when you are a diabetic (person with diabetes) there is a chance of getting your joints amputated (chopped off)? So, take care of your health!',
+                      'Did you know that diabetics (person with diabetes) cannot eat too many carbohydrates like white rice? You would need to talk to your doctor about your diet. So if you like your white rice a lot, take care of your health!',
+                      'Diabetes can cause so many issues: Heart disease, Stroke, Kidney Disease / Kidney Failure, Nerve damage, eye disease, digestive problems',
+                      'Diabetes can also be caused by genetics, but there is no exact cause, no cure or any known prevention.',
+                      'Remember! Eating well, staying active is the best prevention to diseases.'
+                    ];
+        
+        var random_facts = Math.floor((Math.random() * diabetes_facts.length));
+        // console.log(diabetes_facts[random_facts]);
 
-        // GAME OVER (CAN'T FIX): still cannot have the overlay)
-        // this.gameOverText = this.add.text(275, 250, `Game Over`, { fontSize: '32px', fill: '#000' }).setOrigin(0.5);
-        // this.gameOverText.visible = false;
+        var warningSignsDisclaimer = "Diabetes is a serious condition, eat your sweet stuffs moderately.";  
 
-    }
+        const { width, height } = this.scale;
+        this.add.text(width * 0.5, height * 0.4, `${diabetes_facts[random_facts]}`, {
+            fontSize: 25,
+            wordWrap: {
+                width: width*0.8,
+                useAdvancedWrap: true
+            },
+            align: 'center'
+        }).setOrigin(0.5)
 
-    startGame() {
-        this.scene.start('snakeGame')
+        this.add.text(width * 0.5, height * 0.65, `<Press DOWN arrow to continue playing>`, {
+            fontSize: 25,
+            wordWrap: {
+                width: width*0.8,
+                useAdvancedWrap: true
+            },
+            color: "#333A56",
+            stroke: "#52658F",
+            strokeThickness: .5,
+            align: "center",
+        }).setOrigin(0.5);
+
+        this.add.text(width * 0.5, height * 0.8, `*${warningSignsDisclaimer}`, {
+            fontSize: 15,
+            wordWrap: {
+                width: width*0.9,
+                useAdvancedWrap: true
+            },
+            align: 'center',
+            fontStyle: 'italic'
+        }).setOrigin(0.5)
+
+        // Press the spacebar to start the scene, will head to snakePreloader and not snakeGame
+        // as we would need to load the images 1st before running the codes in snakeGame.js
+        this.input.keyboard.once(`keydown-DOWN`, () => {
+            this.scene.start('snakePreloader');
+        })
+
     }
 
 }
--- src/views/snakeGame.vue
+++ src/views/snakeGame.vue
@@ -1,30 +1,25 @@
 <template>
-  <!-- NAVIGATION BAR-->
-  <ul>
-    
-    <li><a href="/"><img src="../assets/kriticalcare(newedit).png" alt="" width="100" height="100"></a></li>
-    <li><a href="/games" width="100" height="100">Games Menu</a></li>
-    <li><a href="/games">About</a></li>
-
-  </ul> 
+  <br>
+  <br>
+  <br>
 
   <!-- GAME CONTAINER -->  
   <div class="background1">
 
     <div class="game-screen">
+
       <div style="color: white; font-family: myFirstFont;">Snake game</div>
-      <div> {{this.score}}; </div>
+      <br>
+      <div style="color: white; font-family: myFirstFont;"> {{this.score}} </div>
+      
       <ion-phaser 
         v-bind:game.prop="game"
         v-bind:initialize.prop="initialize"
       />
-    </div>
 
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+      <div style="color: white; font-family: myFirstFont;">Click into the game to pause/resume</div>      
+
+    </div>
 
   </div>
 
@@ -35,7 +30,7 @@ import Phaser from 'phaser'
 import snakeStart from '../snake_files/snakeStart.js'
 import snakePreloader from '../snake_files/snakepreloader.js'
 import snakeGame from '../snake_files/snakeGame.js'
-// import gameOver from '../snake_files/gameOver.js'
+import gameOver from '../snake_files/gameOver.js'
 import eventsCenter from '../snake_files/EventsCenter.js'
 
 export default {
@@ -47,14 +42,14 @@ export default {
 
   data: function() {
     return {
-      diabetes_score: 0,
+      score: 0,
       initialize: true,
       game: {
         width: 570,
         height: 500,
-        backgroundColor: '#7791a1',
+        backgroundColor: '#d4afb9',
         type: Phaser.AUTO,
-        scene: [snakeStart, snakePreloader, snakeGame]  
+        scene: [snakeStart, snakePreloader, snakeGame, gameOver]  
       },
     }
   },
@@ -94,30 +89,6 @@ export default {
     width:100%;
     height:100%; 
     background-size: cover;
-  }
-
-  ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #5c616e;
-  }
-
-  li {
-    float: left;
-  }
-
-  li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;  
-  }
-  
-  li a:hover {
-    background-color: #7791a1;
   }
 
 </style>
