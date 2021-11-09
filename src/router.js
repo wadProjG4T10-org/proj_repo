45aee608598b75a99d5fbc15a661e7d3eea6290c
ziyi <ziyi@zy-macbook-pro.lan>
2021-11-10 00:55:05 +0800

    information w/o database

--- src/router.js
+++ src/router.js
@@ -6,11 +6,7 @@ import Locator from './views/Locator.vue'
 import SnakeGame from './views/snakeGame.vue'
 import loginPage from "./views/loginPage.vue"
 import createAccount from "./views/createAcc.vue"
-<<<<<<< HEAD
-=======
-import depressionGame from './views/DepressionGame.vue'
-
->>>>>>> 813c3d1d086362143a2e08a8f1d964639664932e
+import Information from "./views/Information.vue"
 
 const routes = [
     { path: '/', component: Home, name: "Home" },
@@ -20,11 +16,7 @@ const routes = [
     { path: '/games/diabetes', component: SnakeGame, name: "Snake Game" },
     { path: '/games/login', component: loginPage, name: "Login Page" },
     { path: '/games/create', component: createAccount, name: "Create Account" },
-<<<<<<< HEAD
-
-=======
-    { path: '/games/depression', component: depressionGame, name: "Depression Game" },
->>>>>>> 813c3d1d086362143a2e08a8f1d964639664932e
+    { path: '/information', component: Information, name: "Information" }
 ]
 
 export default createRouter({
