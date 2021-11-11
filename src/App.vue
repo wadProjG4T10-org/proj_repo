e8ddbd9721d866e8704c1ab3dc1746d41058bb8f
Hazyra <hazyrams.2020@scis.smu.edu.sg>
2021-11-11 23:07:51 +0800

    modified navbar in app page, improved games menu, removed margin-top from snakegame, smaller size icon for the logo

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,47 @@
 <template>
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <div class="container-fluid">
-              <a class="navbar-brand" href="#Home"><img src="./assets/kriticalcare(newedit).png" alt="" width="120" height="114" class="d-inline-block align-text-top"></a>
-          <!--Navbar collapse into an icon -->
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-          </button>
+    <nav class="navbar navbar-light navbar-expand-md sticky-top bg-white justify-content-center">
+        <div class="container">
+            <a class="navbar-brand" href="/"><img src="./assets/kriticalcare-shorter.png" alt="Kritical Care Logo" width="160" height="60" class="d-inline-block align-text-top"></a>
 
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center">
-                  <li class="nav-item text-center">
-                      <a class="nav-link" aria-current="page" href="#">Home</a>
-                  </li>
-
-                  <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Games</a>
-                          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                              <li><a class="dropdown-item" href="#">Heart Disease</a></li>
-                              <li><a class="dropdown-item" href="#">Alzheimer's Disease</a></li>
-                              <li><a class="dropdown-item" href="#">Diabetes</a></li>
-                              <li><a class="dropdown-item" href="#">Depression</a></li>
-                              <li><a class="dropdown-item" href="#">Falls</a></li>
-                          </ul>
-                  </li>
-
-                  <li class="nav-item">
-                      <a class="nav-link" href="#">Information</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link active">Locator</a>
-                  </li>
-              </ul>
-          </div>
-      </div>
-  </nav>
+            <!-- Navbar collapse into an icon -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsingNavbar3">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">
+                <!-- The bar contents to the right -->
+                <ul class="nav navbar-nav ms-auto w-100 justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="/games" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Games </a>
+                        <!-- Games Dropdown -->
+                        <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarScrollingDropdown">
+                            <li><a class="dropdown-item" href="/games">Games Menu</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="/games/alzh">Animatch</a></li>
+                            <li><a class="dropdown-item" href="/games/diabetes">Imma Eat You Up!</a></li>
+                            <li><a class="dropdown-item" href="/games/depression">This is life</a></li>
+                        </ul>
+                    </li>
+                    <!-- Map -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="/map">Map</a>
+                    </li>
+                    <!-- Information -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="/information">Information</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
   <router-view/>
+  <footer> Brought to you by Group 4 Team 10 &#169; Singapore Management University</footer>
+
 </template>
 
 <script>
@@ -46,5 +52,27 @@ export default {
 </script>
 
 <style>
+.navbar {
+    padding-top: 0 !important;
+    padding-bottom: 0 !important;
+    font-size: 20px;
+}
+
+.dropdown:hover .dropdown-menu{
+    display: block;
+    border-radius: 15px;
+}
+
+.navbar-nav li{
+    padding-left:10px;
+    padding-right:10px;
+    margin-left:5px;
+    margin-right:5px;
+}
+
+.navbar-nav li:hover{
+    background-color: #d1cfe2; 
+    border-radius: 20px;
+}
 
 </style>
--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,78 +1,98 @@
 <template>
-  <div class="background1" style="margin-top: 19vh;">
+  <div class="background1">
       <br>
-      <div class="container-fluid">
+      <div class="container">
 
         <!-- GAME MENU TEXT -->
         <b-row>
-          <b-col id="gameHead">Game Menu</b-col>
-        </b-row>
-
-        <br>
-        <!-- OPTION NUMBER 1 -->
-        <b-row style="margin: auto; font-family: Arial; max-width: 30rem;">
-          <!-- ALZHEIMER'S GAME CARD -->
-          <b-col>
-            <GameCard gameName="Alzheimer's Game" 
-              gameDescription="This is a description about Alzheimer's Game which is very very very long."
-              route="/games/alzh"  />
-          </b-col>
-  
-          <!-- SNAKE'S GAME CARD -->
-          <b-col>
-            <GameCard gameName="Snake Game" 
-              gameDescription="Have fun with our Retro Snake Game and learn more about Diabetes through your journey!"
-              route="/games/diabetes"  />
-          </b-col>
-
+          <b-col id="gameHead">Games Menu</b-col>
         </b-row>
         <br>
-        <hr>
-        <br>
 
-        <!-- OPTION NUMBER 2 -->
+        <!-- GAME CARDS -->
         <b-card-group deck rounded>
 
               <!-- ALZHEIMER'S GAME CARD -->
-              <b-card title="Animate">
-                <b-card-sub-title class="mb-2">Alzheimer's Game</b-card-sub-title>
-                <br>
-                <b-card-img :src="require('../assets/gameMenu-memoryicon.png')" fluid alt="Memory Responsive image"></b-card-img>
-                <br>
-                <br>
+              <b-card>
                 <b-card-text>
-                  This is a description about Alzheimer's Game which is very very very long.
+                  <h4 style="font-size: 40px; font-weight:bold;" 
+                      class="colour">
+                    Animatch
+                  </h4>
                 </b-card-text>
+                
                 <br>
-                <b-button class="btn btn-pastel" href="/games/alzh" size="lg">Play Now!</b-button>
+                
+                <b-card-img :src="require('../assets/gameMenu-memoryicon.png')" alt="Memory Responsive image">
+                </b-card-img>
+                
+                <br>
+                <br>
+                
+                <b-card-text>
+                  Test your memory with Animatch and see if you can get the highest score!
+                </b-card-text>
+                
+                <br>
+                
+                <b-button class="btn" style="background-color: #d4afb9 !important;" href="/games/alzh" size="lg">
+                  Play Now!
+                </b-button>
               </b-card>
 
               <!-- SNAKE GAME'S CARD -->
-              <b-card title="Imma Eat You Up!">
-                <b-card-sub-title class="mb-2">Snake Game</b-card-sub-title>
+              <b-card>
+                <b-card-text>
+                  <h4 style="font-size: 40px; font-weight:bold;" 
+                      class="colour">
+                    Imma Eat You Up!
+                  </h4>
+                </b-card-text>
+
                 <br>
-                <b-card-img :src="require('../assets/gameMenu-snakeicon.png')" fluid alt="Snake Responsive image"></b-card-img>
+                
+                <b-card-img :src="require('../assets/gameMenu-snakeicon.png')"  alt="Snake Responsive image">
+                </b-card-img>
+                
                 <br>
                 <br>
+                
                 <b-card-text>
                   Have fun with our Retro Snake Game and learn more about Diabetes through your journey!
                 </b-card-text>
+                
                 <br>
-                <b-button class="btn" href="/games/diabetes" size="lg">Play Now!</b-button>
+                
+                <b-button class="btn" style="background-color: #d4afb9 !important;" href="/games/diabetes" size="lg" >
+                  Play Now!
+                </b-button>
               </b-card>
 
               <!-- DEPRESSION GAME'S CARD -->
-              <b-card title="This is life">
-                <b-card-sub-title class="mb-2">Depression Game</b-card-sub-title>
-                <br>
-                <b-card-img :src="require('../assets/gameMenu-depressionicon.png')" fluid alt="Depression Responsive image"></b-card-img>
-                <br>
-                <br>
+              <b-card>
                 <b-card-text>
-                  Be Happy, this is our education on Depression!
+                  <h4 style="font-size: 40px; font-weight:bold;" 
+                      class="colour">
+                    This is life
+                  </h4>
                 </b-card-text>
+
                 <br>
-                <b-button class="btn" href="/games/alzh" size="lg">Play Now!</b-button>
+
+                <b-card-img :src="require('../assets/gameMenu-depressionicon.png')"  alt="Depression Responsive image">
+                </b-card-img>
+                
+                <br>
+                <br>
+               
+                <b-card-text>
+                  Fond of Super Mario? Try out this game. Be Happy, this is our education on Depression!
+                </b-card-text>
+                
+                <br>
+                <b-button class="btn" style="background-color: #d4afb9 !important;" href="/games/depression" size="lg">
+                  Play Now!
+                </b-button>
               </b-card>
         
         </b-card-group>
@@ -89,14 +109,10 @@
 
 <script>
 
-import GameCard from '../components/GameCard.vue'
 
 export default {
 
   name: 'Games',
-  components: {
-    GameCard
-  }
 
 }
 </script>
@@ -108,7 +124,11 @@ export default {
   }
 
   .background1 {
-    background-color: #9cadce;
+    background-image:
+      linear-gradient(
+        to bottom right, 
+        #d4afb9, #9cadce, #7ec4cf
+    );
     width:100%;
     height:100%; 
     background-size: cover;
@@ -118,22 +138,16 @@ export default {
     font-family: myFirstFont; 
     font-size: 50px;
     color: white;
-    -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-width: 2px;
     -webkit-text-stroke-color: black;
   }
-
-  .row1 {
-        background-color: #d4afb9; /* Dusty Pink */
+  
+  .colour {
+      color: #9cadce;
+      text-shadow: 2px 2px black;
   }
 
-  .cards-container {
-    margin-top: 4rem;
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    row-gap: 1rem;
-  }
-
-  .btn-pastel {
-    background-color: #d4afb9;
+  .btn:hover {
+    color: black !important;
   }
 </style>
