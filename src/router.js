d8043e2e03960832a83f8acb0954b8ede0b8ce86
avigale <80951408+avigale@users.noreply.github.com>
2021-11-12 13:50:20 +0800

    rename file

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import AlzheimersGame from './views/AlzhGame.vue'
 import Games from './views/Games.vue'
-import Locator from './views/Locator.vue'
+import Map from './views/Map.vue'
 import SnakeGame from './views/snakeGame.vue'
 import loginPage from "./views/loginPage.vue"
 import createAccount from "./views/createAcc.vue"
@@ -14,7 +14,7 @@ const routes = [
     { path: '/', component: Home, name: "Home" },
     { path: '/games', component: Games, name: "Games" },
     { path: '/games/alzh', component: AlzheimersGame, name: "Alzheimer's Game" },
-    { path: '/map', component: Locator, name: "Locator"},
+    { path: '/map', component: Map, name: "Map"},
     { path: '/games/diabetes', component: SnakeGame, name: "Snake Game" },
     { path: '/games/login', component: loginPage, name: "Login Page" },
     { path: '/games/create', component: createAccount, name: "Create Account" },
